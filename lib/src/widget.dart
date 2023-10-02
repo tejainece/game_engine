9ea41ca2a136cbc1f3bdb4ef7dfd9cc515c2d0ac
Ravi Teja Gudapati <tejainece@gmail.com>
2023-10-02 18:51:51 +0200

    WIP: GameWidget PanZoom events

--- lib/src/widget.dart
+++ lib/src/widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:game_engine/game_engine.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -5,29 +6,36 @@ import 'package:flutter/scheduler.dart';
 
 class GameWidget extends LeafRenderObjectWidget {
   final ValueChanged<Size>? onResize;
+  final ValueChanged<PointerEvent>? onPanZoom;
   final List<List<Component>> components;
 
-  const GameWidget({Key? key, this.onResize, required this.components})
+  const GameWidget(
+      {Key? key, this.onResize, this.onPanZoom, required this.components})
       : super(key: key);
 
   @override
   RenderObject createRenderObject(BuildContext context) =>
-      GameWidgetRenderObject(onResize: onResize, components: components);
+      GameWidgetRenderObject(
+          onResize: onResize, components: components, onPanZoom: onPanZoom);
 
   @override
   void updateRenderObject(
       BuildContext context, GameWidgetRenderObject renderObject) {
-    renderObject._update(onResize: onResize, components: components);
+    renderObject._update(
+        onResize: onResize, onPanZoom: onPanZoom, components: components);
   }
 }
 
 class GameWidgetRenderObject extends RenderBox {
   Size? _oldSize;
   ValueChanged<Size>? _onResize;
+  ValueChanged<PointerEvent>? onPanZoom;
   List<List<Component>> _components;
 
   GameWidgetRenderObject(
-      {ValueChanged<Size>? onResize, required List<List<Component>> components})
+      {ValueChanged<Size>? onResize,
+      this.onPanZoom,
+      required List<List<Component>> components})
       : _components = components {
     this.onResize = onResize;
   }
@@ -41,8 +49,10 @@ class GameWidgetRenderObject extends RenderBox {
 
   void _update(
       {required ValueChanged<Size>? onResize,
+      required ValueChanged<PointerEvent>? onPanZoom,
       required List<List<Component>> components}) {
     _onResize = onResize;
+    this.onPanZoom = onPanZoom;
     _components = components;
     markNeedsPaint();
   }
@@ -81,6 +91,13 @@ class GameWidgetRenderObject extends RenderBox {
 
   @override
   void handleEvent(PointerEvent event, covariant HitTestEntry entry) {
+    if (event is PointerPanZoomStartEvent ||
+        event is PointerPanZoomUpdateEvent ||
+        event is PointerPanZoomEndEvent) {
+      // TODO way to block propagation?
+      onPanZoom?.call(event);
+    }
+
     for (final layer in _components) {
       for (final component in layer) {
         component.handlePointerEvent(event);
@@ -97,7 +114,7 @@ class GameWidgetRenderObject extends RenderBox {
 
   void _tick(Duration elapsed) {
     var delta = const Duration();
-    if(_previousTick != null) {
+    if (_previousTick != null) {
       delta = elapsed - _previousTick!;
     }
     _previousTick = elapsed;
