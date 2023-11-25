365091ca289c898c5f26b689af7b7ce7580b7f1b
Ravi Teja Gudapati <tejainece@gmail.com>
2023-11-25 18:11:00 +0100

    Flex RowComponent

--- lib/src/flex/row.dart
+++ lib/src/flex/row.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:game_engine/game_engine.dart';
+import 'package:collection/collection.dart';
 
 abstract class FlexComponent implements Component {
   void set({Offset? offset});
@@ -9,54 +10,250 @@ abstract class FlexComponent implements Component {
   Size get size;
 }
 
-class RowComponent implements Component {
-  var _children = <FlexComponent>[];
+abstract class DimensionedComponent implements Component {
+  Offset get offset;
+
+  Size get size;
+
+  void set({Offset? offset, Size? size});
+}
+
+class _Child {
+  final FlexComponent component;
+  Size? size;
+
+  _Child({required this.component, this.size});
+}
+
+class RowComponent implements Component, FlexComponent, DimensionedComponent {
+  DimensionedComponent? _bg;
+  var _children = <_Child>[];
   Offset _offset = const Offset(0, 0);
   Size _size = const Size(0, 0);
   var _crossAxisAlign = CrossAxisAlignment.start;
   var _align = MainAxisAlignment.start;
 
   RowComponent(
-      {required List<Component> children,
+      {required List<FlexComponent> children,
+        required DimensionedComponent? bg,
       required Offset offset,
       required Size size,
       CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.start,
       MainAxisAlignment align = MainAxisAlignment.start}) {
-    _offset = offset;
-    _size = size;
+    _bg = bg;
     _crossAxisAlign = crossAxisAlignment;
     _align = align;
+
+    set(offset: offset, size: size);
   }
 
   @override
-  void handlePointerEvent(PointerEvent event) {
-    for (final child in _children) {
-      child.handlePointerEvent(event);
-    }
-  }
-
+  Size get size => _size;
   @override
-  void render(Canvas canvas) {
-    for (final child in _children) {
-      child.render(canvas);
-    }
-  }
+  Offset get offset => _offset;
 
-  @override
-  void tick(TickCtx ctx) {
-    for (final child in _children) {
-      child.tick(ctx);
+  Iterable<FlexComponent> get children => _children.map((e) => e.component);
+  set children(Iterable<FlexComponent> children) {
+    if(_compareChildren(children)) return;
+
+    _children.clear();
+    for (final child in children) {
+      _children.add(_Child(component: child, size: child.size));
     }
 
     _layout();
   }
 
+  bool _compareChildren(Iterable<FlexComponent> children) {
+    if(children.length != _children.length) {
+      return false;
+    }
+
+    for(final pair in IterableZip([children, this.children])) {
+      if(pair[0] != pair[0]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  @override
+  void set({Offset? offset, Size? size, CrossAxisAlignment? crossAxisAlignment, MainAxisAlignment? align, List<FlexComponent>? children}) {
+    bool needsLayout = false;
+    bool dimChanged = false;
+    if(offset != null && offset != _offset) {
+      _offset = offset;
+      needsLayout = true;
+      dimChanged = true;
+    }
+    if(size != null && size != _size) {
+      _size = size;
+      needsLayout = true;
+      dimChanged = true;
+    }
+    if(dimChanged) {
+      _bg?.set(offset: _offset, size: _size);
+    }
+    if(crossAxisAlignment != null && crossAxisAlignment != _crossAxisAlign) {
+      _crossAxisAlign = crossAxisAlignment;
+      needsLayout = true;
+    }
+    if(align != null && align != _align) {
+      _align = align;
+      needsLayout = true;
+    }
+    if(children != null && !_compareChildren(children)) {
+      _children.clear();
+      for (final child in children) {
+        _children.add(_Child(component: child));
+      }
+      needsLayout = true;
+    }
+    if(needsLayout) {
+      _bg?.set(offset: _offset, size: _size);
+      _layout();
+    }
+  }
+
+  @override
+  void handlePointerEvent(PointerEvent event) {
+    _bg?.handlePointerEvent(event);
+    for (final child in _children) {
+      child.component.handlePointerEvent(event);
+    }
+  }
+
+  @override
+  void render(Canvas canvas) {
+    _bg?.render(canvas);
+    for (final child in _children) {
+      child.component.render(canvas);
+    }
+  }
+
+  @override
+  void tick(TickCtx ctx) {
+    _bg?.tick(ctx);
+    bool shouldLayout = false;
+    for (final child in _children) {
+      child.component.tick(ctx);
+      if (child.size != child.component.size) {
+        child.size = child.component.size;
+        shouldLayout = true;
+      }
+    }
+
+    if (shouldLayout) {
+      _layout();
+    }
+  }
+
   void _layout() {
-    if(_align == MainAxisAlignment.start) {
-      double offset = 0;
-      for(final child in _children) {
-        child.set(offset: Offset(dx, dy));
-        offset += child.;
+    // TODO implement wrapping
+    if (_align == MainAxisAlignment.start) {
+      double offset = _offset.dx;
+      for (final child in _children) {
+        double dy = _offset.dy;
+        if (_crossAxisAlign == CrossAxisAlignment.center) {
+          dy += (_size.height - child.component.size.height) / 2;
+        } else if (_crossAxisAlign == CrossAxisAlignment.end) {
+          dy += _size.height - child.component.size.height;
+        } else if (_crossAxisAlign == CrossAxisAlignment.stretch) {
+          // TODO
+        }
+
+        final tmp = offset + child.component.size.width;
+        child.component.set(offset: Offset(tmp, dy));
+        offset = tmp;
+      }
+    } else if (_align == MainAxisAlignment.end) {
+      double offset = _offset.dx + _size.width;
+      for (final child in _children) {
+        double dy = _offset.dy;
+        if (_crossAxisAlign == CrossAxisAlignment.center) {
+          dy += (_size.height - child.component.size.height) / 2;
+        } else if (_crossAxisAlign == CrossAxisAlignment.end) {
+          dy += _size.height - child.component.size.height;
+        } else if (_crossAxisAlign == CrossAxisAlignment.stretch) {
+          // TODO
+        }
+
+        final tmp = offset - child.component.size.width;
+        child.component.set(offset: Offset(tmp, dy));
+        offset = tmp;
+      }
+    } else if (_align == MainAxisAlignment.center) {
+      double totalWidth =
+          _children.fold(0.0, (p, e) => p + e.component.size.width);
+      var offset = _offset.dx + (_size.width - totalWidth) / 2;
+      for (final child in _children) {
+        double dy = _offset.dy;
+        if (_crossAxisAlign == CrossAxisAlignment.center) {
+          dy += (_size.height - child.component.size.height) / 2;
+        } else if (_crossAxisAlign == CrossAxisAlignment.end) {
+          dy += _size.height - child.component.size.height;
+        } else if (_crossAxisAlign == CrossAxisAlignment.stretch) {
+          // TODO
+        }
+
+        final tmp = offset + child.component.size.width;
+        child.component.set(offset: Offset(tmp, dy));
+        offset = tmp;
+      }
+    } else if (_align == MainAxisAlignment.spaceBetween) {
+      final totWid = _children.fold(0.0, (p, e) => p + e.component.size.width);
+      final space = (_size.width - totWid) / (_children.length - 1);
+
+      double dx = _offset.dx;
+      for (final child in _children) {
+        double dy = _offset.dy;
+        if (_crossAxisAlign == CrossAxisAlignment.center) {
+          dy += (_size.height - child.component.size.height) / 2;
+        } else if (_crossAxisAlign == CrossAxisAlignment.end) {
+          dy += _size.height - child.component.size.height;
+        } else if (_crossAxisAlign == CrossAxisAlignment.stretch) {
+          // TODO
+        }
+
+        child.component.set(offset: Offset(dx, dy));
+        dx += child.component.size.width + space;
+      }
+    } else if (_align == MainAxisAlignment.spaceAround) {
+      final totWid = _children.fold(0.0, (p, e) => p + e.component.size.width);
+      final space = (_size.width - totWid) / (_children.length * 2);
+
+      double offset = _offset.dx + space;
+      for (final child in _children) {
+        double dy = _offset.dy;
+        if (_crossAxisAlign == CrossAxisAlignment.center) {
+          dy += (_size.height - child.component.size.height) / 2;
+        } else if (_crossAxisAlign == CrossAxisAlignment.end) {
+          dy += _size.height - child.component.size.height;
+        } else if (_crossAxisAlign == CrossAxisAlignment.stretch) {
+          // TODO
+        }
+
+        child.component.set(offset: Offset(offset, dy));
+        offset += child.component.size.width + space * 2;
+      }
+    } else if (_align == MainAxisAlignment.spaceEvenly) {
+      final totWid = _children.fold(0.0, (p, e) => p + e.component.size.width);
+      final space = (_size.width - totWid) / (_children.length + 1);
+
+      double dx = _offset.dx + space;
+      for (final child in _children) {
+        double dy = _offset.dy;
+        if (_crossAxisAlign == CrossAxisAlignment.center) {
+          dy += (_size.height - child.component.size.height) / 2;
+        } else if (_crossAxisAlign == CrossAxisAlignment.end) {
+          dy += _size.height - child.component.size.height;
+        } else if (_crossAxisAlign == CrossAxisAlignment.stretch) {
+          // TODO
+        }
+
+        child.component.set(offset: Offset(dx, dy));
+        dx += child.component.size.width + space;
       }
     }
   }
